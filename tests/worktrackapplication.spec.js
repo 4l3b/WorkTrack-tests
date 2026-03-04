3b4a7d498c206f3e41938b6ece2b931cb4cda3d6
4l3b <alessandro.bigolin.fdi@gmail.com>
2026-03-04 11:48:35 +0100

    Create test for session start functionality
    Add test for starting a session using Page Object Model.

---
+++ tests/worktrackapplication.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+const WorkTrackPage = require("../pages/workTrackPage")
+
+
+test('Start Session Using POM', async ({ page }) =>
+{
+	await page.goto('https://4l3b.github.io/WorkTrack/workTrack.html')
+
+	const worktrackPage = new WorkTrackPage(page)
+	await worktrackPage.verifyClockOutTimeLeftBeforeSession()
+	await worktrackPage.startSessionManually()
+	await worktrackPage.verifyClockOutTimeLeft()
+	
+});
