0216ce5b3b01b141a074e331dbbad48c2b85cec8
4l3b <alessandro.bigolin.fdi@gmail.com>
2026-03-04 11:49:53 +0100

    Add WorkTrackPage class for time tracking functionality

---
+++ pages/worktrackPage.js
@@ -0,0 +1,70 @@
+const {expect} = require("@playwright/test")
+
+class WorkTrackPage
+{
+	
+	constructor(page)
+	{	
+			
+		this.page = page
+		
+		this.worktimeSideBtn = "#worktime-btn"
+		this.worktimeBtnPopup = "#worktime-btn-popup"
+		this.worktimePopupHours = "#hours-worktime"
+		this.worktimePopupMinutes = "#minutes-worktime"
+		this.worktimePopupSeconds = "#seconds-worktime"
+		
+		this.breakminSideBtn = "#breakmin-btn"
+		this.breakminBtnPopup = "#breakmin-btn-popup"
+		this.breakminPopupHours = "#hours-breakmin"
+		this.breakminPopupMinutes = "#minutes-breakmin"
+		
+		this.clockinSideBtn = "#enterclockin-btn"
+		this.clockinBtnPopup = "#clockin-btn-popup"
+		this.clockinPopupHours = "#hours-clockin"
+		this.clockinPopupMinutes = "#minutes-clockin"
+		this.clockinPopupSeconds = "#seconds-clockin"
+		
+		this.clockOutField = "#clock-out"
+		this.timeLeftField = "#countdown"
+		
+	}	
+	
+	async startSessionManually()
+	{
+	
+		await this.page.click(this.worktimeBtnPopup)
+		await this.page.fill(this.worktimePopupHours, "08")
+		await this.page.fill(this.worktimePopupMinutes, "00")
+		await this.page.fill(this.worktimePopupSeconds, "00")
+		
+		await this.page.click(this.breakminBtnPopup)
+		await this.page.fill(this.breakminPopupHours, "00")
+		await this.page.fill(this.breakminPopupMinutes, "30")
+		
+		await this.page.click(this.clockinBtnPopup)
+		await this.page.fill(this.clockinPopupHours, "08")
+		await this.page.fill(this.clockinPopupMinutes, "00")
+		await this.page.fill(this.clockinPopupSeconds, "00")
+			
+	}
+	
+	
+	async verifyClockOutTimeLeft()
+	{
+	
+		await expect(this.page.locator(this.clockOutField)).not.toHaveText(/.*CLOCK OUT.*--:--:--/, { timeout: 4000 })
+		await expect(this.page.locator(this.timeLeftField)).not.toHaveText(/.*TIME LEFT.*--:--:--/, { timeout: 4000 })
+	
+	}
+	
+	
+	async verifyClockOutTimeLeftBeforeSession()
+	{
+		await expect(this.page.locator(this.clockOutField)).toHaveText(/.*CLOCK OUT.*--:--:--/)
+		await expect(this.page.locator(this.timeLeftField)).toHaveText(/.*TIME LEFT.*--:--:--/)
+	}
+
+}
+
+module.exports = WorkTrackPage
